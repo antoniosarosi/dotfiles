b60bbcfa6967dfe586b411cfb6fb65309d2d5a9b
Antonio Sarosi <sarosiantonio@gmail.com>
2020-04-13 01:19:33 +0200

    change some code format

--- .config/alacritty/theme.py
+++ .config/alacritty/theme.py
@@ -9,23 +9,24 @@ def change_theme(alacritty_file, theme_file):
             alacritty = yaml.load(f, Loader=yaml.FullLoader)
         with open(theme_file) as f:
             theme = yaml.load(f, Loader=yaml.FullLoader)
-         
-        
-        if "colors" in theme:
-            alacritty["colors"] = theme["colors"]
-        else:
-            print("Theme \"{theme_file}\" has no color configuration.")
+
+        if "colors" not in theme:
+            print('Theme "{theme_file}" has no color configuration')
             return False
+
+        alacritty["colors"] = theme["colors"]
 
         with open(alacritty_file, "w") as f:
             yaml.dump(alacritty, f)
-                  
-    except PermissionError as e:
-        print("Can't read/write {0.filename} : Not allowed".format(e))
-    except yaml.YAMLError as e:
-        print("YAML error at parsing file:\n at line {0.problem_mark.line}, column {0.problem_mark.column} : {0.problem} {0.context}".format(e))
-    else:
+
         return True
+
+    except yaml.YAMLError as e:
+        print("YAML error at parsing file, ", end="")
+        print("at line {0.problem_mark.line}, ".format(e), end="")
+        print("column {0.problem_mark.column}:".format(e))
+        print("{0.problem} {0.context}\n".format(e))
+
     return False
 
 
@@ -33,19 +34,19 @@ def main():
     if len(argv) != 2:
         print("Usage:\npython {} theme.yaml".format(argv[0]))
         exit(0)
-    
+
     if not path.exists(argv[1]):
         print("Theme file {} does not exist".format(argv[1]))
         exit(0)
 
     alacritty_path = path.join(
-        path.expanduser("~"),
-        ".config/alacritty/alacritty.yml"
+        path.expanduser("~"), ".config", "alacritty", "alacritty.yml"
     )
+
     if change_theme(alacritty_path, argv[1]):
         print("Theme successfully changed")
     else:
-        print("Could not change theme.")
+        print("Could not change theme")
 
 
 if __name__ == "__main__":
