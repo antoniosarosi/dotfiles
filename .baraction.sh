65e1233f450907b5d6f7617cd750d60c80a67eae
Antonio Sarosi <sarosiantonio@gmail.com>
2020-04-16 14:04:32 +0200

    Add volume to spectrwm bar

--- .baraction.sh
+++ .baraction.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # baraction.sh for spectrwm status bar
 
-## DATE
+# DATE
 dte() {
   dte="$(date +"%d / %m / %Y  - %H:%M")"
   echo -e "$dte"
 }
 
-## DISK
+# DISK
 # hdd() {
 #   hdd="$(df -h | awk 'NR==4{print $3, $5}')"
 #   echo -e "HDD: $hdd"
 # }
 
-## RAM
+# RAM
 mem() {
   mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
   echo -e "MEM: $mem"
 }
 
-## CPU
+# CPU
 cpu() {
   read cpu a b c previdle rest < /proc/stat
   prevtotal=$((a+b+c+previdle))
@@ -30,29 +30,29 @@ cpu() {
   echo -e "CPU: $cpu%"
 }
 
-## Batery
+# Batery
 bat() {
   bat=`upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage | sed 's/ *percentage: *//g'`
   echo -e "Battery: $bat"
 }
 
-## Bright
+# Bright
 br() {
   br=`brightnessctl | grep Current | sed "s/ *Current b/B/" | sed "s/\t//"`
   echo -e "$br"
 }
 
-## 
+# Volume
+vol() {
+  vol=`pamixer --get-volume`
+  echo -e "Volume: $vol%"
+}
 
-## VOLUME
-# vol() {
-#    vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }'`
-#    echo -e "VOL: $vol"
-# }
+
 
 SLEEP_SEC=5
 #loops forever outputting a line every SLEEP_SEC secs
 while :; do
-    echo "$(cpu)    |    $(mem)    |    $(bat)    |    $(br)    |    $(dte)"
+    echo "$(cpu)    |    $(mem)    |    $(bat)    |    $(br)    |    $(vol)    |    $(dte)"
 	sleep $SLEEP_SEC
 done
