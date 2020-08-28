5c01bd8dc3f1633f18f9cdfb5dfd527bbd9c3bab
Antonio Sarosi <sarosiantonio@gmail.com>
2020-08-28 17:10:55 +0200

    Dwm config

---
+++ .dwm/README.md
@@ -0,0 +1,65 @@
+My custom and patched build of **[dwm](https://dwm.suckless.org/)**.
+
+Patches:
+- [autostart](https://dwm.suckless.org/patches/autostart/dwm-autostart-20200610-cb3f58a.diff)
+- [restartsig](https://dwm.suckless.org/patches/restartsig/dwm-restartsig-20180523-6.2.diff)
+- [attachaside](https://dwm.suckless.org/patches/attachaside/dwm-attachaside-6.1.diff)
+- [focusonactive](https://dwm.suckless.org/patches/focusonnetactive/dwm-focusonnetactive-6.2.diff)
+- [warp](https://dwm.suckless.org/patches/warp/dwm-warp-6.2.diff)
+- [rotatestack](https://dwm.suckless.org/patches/rotatestack/dwm-rotatestack-20161021-ab9571b.diff)
+- [systray](https://dwm.suckless.org/patches/systray/dwm-systray-20200610-f09418b.diff)
+- [scheme switch](https://dwm.suckless.org/patches/scheme_switch/dwm-scheme_switch-20170804-ceac8c9.diff)
+- [fullgaps](https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff)
+- [gridmode](https://dwm.suckless.org/patches/gridmode/dwm-gridmode-20170909-ceac8c9.diff)
+- [columns](https://dwm.suckless.org/patches/columns/dwm-columns-6.0.diff)
+- [cyclelayouts](https://dwm.suckless.org/patches/cyclelayouts/dwm-cyclelayouts-20180524-6.2.diff)
+
+To install this on your system, first you need the following dependencies:
+
+```bash
+sudo pacman -S pacman-contrib
+yay -S nerd-fonts-ubuntu-mono
+```
+
+I always use that font for icons. *pacman-contrib* is used for checking updates.
+You will also need my custom
+**[dwmblocks](https://github.com/antoniosarosi/dotfiles/tree/master/.config/dwmblocks)**
+and **[~/.local/bin](https://github.com/antoniosarosi/dotfiles/tree/master/.local/bin)**
+scripts.
+
+```bash
+git clone https://github.com/antoniosarosi/dotfiles.git
+cp -r dotfiles/.dwm ~
+cp -r dotfiles/.config/dwmblocks ~/.config/
+cp dotfiles/.local/bin/battery ~/.local/bin/
+cp dotfiles/.local/bin/brightness ~/.local/bin/
+```
+
+Place this in your **~/.xprofile**:
+
+```bash
+export PATH=$HOME/.local/bin:$PATH
+```
+
+Build *dwm* and *dwmblocks* and create a new *xsession*:
+
+```bash
+cd ~/.dwm
+sudo make clean install
+cd ~/.config/dwmblocks/
+sudo make clean install
+sudo cp ~/.dwm/dwm.desktop /usr/share/xsessions
+```
+
+Test it with **[Xephyr](https://wiki.archlinux.org/index.php/Xephyr)**:
+
+```bash
+Xephyr -br -ac -noreset -screen 1280x720 :1 &
+DISPLAY=:1 dwm
+```
+
+Once that's done, you can login. But keep in mind keybindings will not work
+unless you have the same programs that I use and the same configs. You can
+either change keybindings or install the software I use and my config files,
+check out [this section](https://github.com/antoniosarosi/dotfiles#keybindings)
+for instructions.
--- .dwm/config.h
+++ .dwm/config.h
@@ -1,144 +1,222 @@
-/* See LICENSE file for copyright and license details. */
+// -------------------------------- Appearance ---------------------------------
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=14:weight=bold:antialias=true:autohint:true" };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=12:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#4c566a";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#5638d9";
-static const char test[] = "#E06C75";
+// border pixel of windows
+static const unsigned int borderpx  = 1;
+// gaps between windows 
+static const unsigned int gappx = 6;
+// snap pixel 
+static const unsigned int snap = 32;
+// 0: sloppy systray follows selected monitor, >0: pin systray to monitor X 
+static const unsigned int systraypinning = 0;
+// systray spacing 
+static const unsigned int systrayspacing = 2;
+// 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor
+static const int systraypinningfailfirst = 1;
+// 0 means no systray
+static const int showsystray = 1;
+// 0 means no bar 
+static const int showbar = 1;
+// 0 means bottom bar 
+static const int topbar = 1;
+
+// ---------------------------------- Fonts ------------------------------------
+
+static const char *fonts[] = { "UbuntuMono Nerd Font:size=14:weight=bold:antialias=true:autohint:true" };
+static const char dmenufont[] = "UbuntuMono Nerd Font:size=12:antialias=true:autohint=true";
+
+// ---------------------------------- Colors -----------------------------------
+
+static const char window_border[] = "#000000";
+static const char inactive[] = "#4c566a";
+static const char active[] = "#ffffff";
+
+static const char material_bg[] = "#0f101a";
+static const char material_focus[] = "#a151d3";
+
+static const char onedark_bg[] = "#1e2127";
+static const char onedark_focus[] = "#E06C75";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	{ col_gray3, col_gray1, col_gray2 }, /* SchemeNorm orig */
-	{ col_gray4, test,  test  }, /* SchemeSel orig */
-	{ col_gray3, col_gray1, col_gray2 }, /* SchemeNorm orig */
-	{ col_gray4, col_cyan,  col_cyan  }, /* SchemeSel orig */
+    // fg        bg         border   
+    { inactive, material_bg, window_border },
+    { active, material_focus,  material_focus  },
+
+    { inactive, onedark_bg, window_border },
+    { active, onedark_focus,  onedark_focus  },
 };
 
-/* tagging */
-static const char *tags[] = { "   ", "  ", "   ", "   ", "   ", "   ", "   ", "   ", "   " };
+// -------------------------------- Workspaces ---------------------------------
+
+static const char *tags[] = { "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ", "   " };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    /* xprop(1):
+     * WM_CLASS(STRING) = instance, class
+     * WM_NAME(STRING) = title
+     */
+
+    // class      instance    title       tags mask     isfloating   monitor 
+    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
-/* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+// ---------------------------------- Layouts ----------------------------------
+
+static const float mfact = 0.50;  // factor of master area size [0.05..0.95] 
+static const int nmaster = 1;     // number of clients in master area 
+static const int resizehints = 1; // 1 means respect size hints in tiled resizals 
 
 #include "layouts.c"
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]",       tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
-    { "[C]" ,     tcl },
-	{ "[G]",      grid },
-    { NULL, NULL },
+    // symbol   arrange function
+    { "[]",     tile }, // first entry is default 
+    { "[F]",    NULL }, // no layout function means floating behavior 
+    { "[M]",    monocle },
+    { "[C]",    tcl },
+    { "[G]",    grid },
+    { NULL,     NULL },
 };
 
-/* key definitions */
-#define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+// -------------------------------- Keybindings --------------------------------
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define MODKEY Mod4Mask
+
+#define TAGKEYS(KEY,TAG) \
+    { MODKEY,                       KEY,  view,       {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask,           KEY,  toggleview, {.ui = 1 << TAG} }, \
+    { MODKEY|ShiftMask,             KEY,  tag,        {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask|ShiftMask, KEY,  toggletag,  {.ui = 1 << TAG} },
+
+// Helper for spawning shell commands in the pre dwm-5.0 fashion 
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// Commands
+static char dmenumon[2] = "0";
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m", dmenumon,
+    "-fn", dmenufont,
+    NULL
+};
 
 #include <X11/XF86keysym.h>
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,                       XK_b,      togglebar,      {0} },
-    { MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,             XK_w,      killclient,     {0} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      spawn,           SHCMD("rofi -show run") },
-	{ MODKEY|ShiftMask,                        XK_m,      spawn, SHCMD("rofi -show") },
-	{ MODKEY,                        XK_e,      spawn,           SHCMD("thunar") },
-	{ MODKEY,                        XK_b,      spawn,           SHCMD("firefox") },
-	{ MODKEY,                        XK_r,      spawn,           SHCMD("redshift -O 2400") },
-	{ MODKEY|ShiftMask,                        XK_r,      spawn, SHCMD("redshift -x") },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    { MODKEY,                       XK_g,  setgaps,        {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_g,  setgaps,        {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_t,      schemeToggle,   {0} },
-    { MODKEY|ShiftMask,             XK_z,      schemeCycle,    {0} },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ControlMask,             XK_q,      quit,           {0} },
-    { MODKEY|ControlMask,           XK_r,      quit,           {1} }, 
-    { MODKEY|ShiftMask, 		    XK_Tab,  cyclelayout,    {.i = -1 } },
-	{ MODKEY,           XK_Tab, cyclelayout,    {.i = +1 } },
 
-    {0, XF86XK_MonBrightnessUp, spawn, SHCMD("brightnessctl set +10%")},
-    {0, XF86XK_MonBrightnessDown, spawn, SHCMD("brightnessctl set 10%-")},
+    // ------------------ Windows ------------------
+
+    // Switch between windows
+    { MODKEY, XK_j, focusstack, {.i = +1 } },
+    { MODKEY, XK_k, focusstack, {.i = -1 } },
+
+    // Change window sizes
+    { MODKEY, XK_i, incnmaster, {.i = +1 } },
+    { MODKEY|ShiftMask, XK_i, incnmaster, {.i = -1 } },
+    { MODKEY|ShiftMask, XK_h, setmfact, {.f = -0.05} },
+    { MODKEY|ShiftMask, XK_l, setmfact, {.f = +0.05} },
+
+    // Toggle floating
+    { MODKEY|ShiftMask, XK_f, togglefloating, {0} },
+
+    // Toggle bar
+    { MODKEY|ShiftMask, XK_b, togglebar, {0} },
+
+    // Move windows in current stack
+    { MODKEY|ShiftMask, XK_j, rotatestack, {.i = +1 } },
+    { MODKEY|ShiftMask, XK_k, rotatestack, {.i = -1 } },
+
+    // Cycle layouts
+    { MODKEY, XK_Tab, cyclelayout, {.i = +1 } },
+    { MODKEY|ShiftMask, XK_Tab, cyclelayout,  {.i = -1 } },
+
+    // Increase - decrease gaps
+    { MODKEY, XK_g, setgaps, {.i = -1 } },
+    { MODKEY|ShiftMask, XK_g, setgaps, {.i = +1 } },
+
+    // Foucs next - prev monitor
+    { MODKEY, XK_period, focusmon, {.i = +1 } },
+    { MODKEY, XK_comma, focusmon, {.i = -1 } },
+
+    // Send window to next - prev monitor
+    { MODKEY|ShiftMask, XK_period, tagmon, {.i = +1 } },
+    { MODKEY|ShiftMask, XK_comma, tagmon, {.i = -1 } },
+
+    // Change color scheme
+    { MODKEY, XK_Left, schemeToggle, {0} },
+    { MODKEY, XK_Right, schemeCycle, {0} },
+
+    // Kill window
+    { MODKEY, XK_w, killclient, {0} },
+
+    // Restart dwm
+    { MODKEY|ControlMask, XK_r, quit, {1} },
+
+    // Quit dwm
+    { MODKEY|ControlMask, XK_q, quit, {0} },
+
+    // ---------------- Workspaces -----------------
+
+    TAGKEYS(XK_1, 0)
+    TAGKEYS(XK_2, 1)
+    TAGKEYS(XK_3, 2)
+    TAGKEYS(XK_4, 3)
+    TAGKEYS(XK_5, 4)
+    TAGKEYS(XK_6, 5)
+    TAGKEYS(XK_7, 6)
+    TAGKEYS(XK_8, 7)
+    TAGKEYS(XK_9, 8)
+
+    // ------------------- Apps --------------------
+
+    // dmenu
+    { MODKEY|ShiftMask, XK_Return, spawn, {.v = dmenucmd } },
+
+    // rofi
+    { MODKEY, XK_m, spawn, SHCMD("rofi -show drun") },
+
+    // Window nav (rofi)
+    { MODKEY|ShiftMask, XK_m, spawn, SHCMD("rofi -show") },
+
+    // Terminal
+    { MODKEY, XK_Return, spawn, SHCMD("alacritty") },
+
+    // File explorer
+    { MODKEY, XK_e, spawn, SHCMD("thunar") },
+
+    // Browser
+    { MODKEY, XK_b, spawn, SHCMD("firefox") },
+
+    // Redshift
+    { MODKEY, XK_r, spawn, SHCMD("redshift -O 2400") },
+    { MODKEY|ShiftMask, XK_r, spawn, SHCMD("redshift -x") },
+
+    // Screenshot
+    { MODKEY, XK_s, spawn, SHCMD("scrot") },
+
+    // ----------------- Hardware ------------------
+
+    // Volume
     {0, XF86XK_AudioLowerVolume, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%")},
     {0, XF86XK_AudioRaiseVolume, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%")},
     {0, XF86XK_AudioMute, spawn, SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle")},
+
+    // Brightness
+    {0, XF86XK_MonBrightnessUp, spawn, SHCMD("brightnessctl set +10%")},
+    {0, XF86XK_MonBrightnessDown, spawn, SHCMD("brightnessctl set 10%-")},
+
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+// click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin 
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    // click            event mask    button       function         argument 
+    { ClkLtSymbol,      0,            Button1,     setlayout,       {0} },
+    { ClkLtSymbol,      0,            Button3,     setlayout,       {.v = &layouts[2]} },
+    { ClkWinTitle,      0,            Button2,     zoom,            {0} },
+    { ClkStatusText,    0,            Button2,     spawn,           {.v = termcmd } },
+    { ClkClientWin,     MODKEY,       Button1,     movemouse,       {0} },
+    { ClkClientWin,     MODKEY,       Button2,     togglefloating,  {0} },
+    { ClkClientWin,     MODKEY,       Button3,     resizemouse,     {0} },
+    { ClkTagBar,        0,            Button1,     view,            {0} },
+    { ClkTagBar,        0,            Button3,     toggleview,      {0} },
+    { ClkTagBar,        MODKEY,       Button1,     tag,             {0} },
+    { ClkTagBar,        MODKEY,       Button3,     toggletag,       {0} },
 };
