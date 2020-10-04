d085e4eb36a42ac6396310d18d8545df04552f14
Antonio Sarosi <sarosiantonio@gmail.com>
2020-10-04 23:59:10 +0200

    Fish config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ alias tree "exa -T"
 
 set -g theme_display_user yes
 set -g theme_hide_hostname no
-set -g color_user_bg red
+set -g color_user_bg magenta
 set -g color_user_str black
 
 # Spacefish
@@ -20,3 +20,7 @@ set SPACEFISH_PROMPT_ADD_NEWLINE false
 set SPACEFISH_PROMPT_PREFIXES_SHOW false
 set SPACEFISH_PROMPT_DEFAULT_PREFIX " "
 set SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
+set SPACEFISH_USER_SHOW always
+set SPACEFISH_USER_COLOR green
+set SPACEFISH_HOST_SHOW always
+set SPACEFISH_PROMPT_ORDER time user host dir git package node ruby golang php rust haskell julia elixir docker aws venv conda pyenv dotnet kubecontext exec_time line_sep battery vi_mode jobs exit_code char  
