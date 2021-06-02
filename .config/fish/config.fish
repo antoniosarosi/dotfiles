18db55400551c231c9e86058b8bba7cd3a25f7f0
Antonio Sarosi <sarosiantonio@gmail.com>
2021-06-02 13:39:42 +0200

    Drop omf and use starship for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,22 +8,6 @@ alias ls "exa --group-directories-first"
 alias tree "exa -T"
 alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
 
-# Agnoster
+# Prompt
 
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g color_user_bg magenta
-set -g color_user_str black
-
-# Spacefish
-
-set SPACEFISH_PROMPT_ADD_NEWLINE false
-set SPACEFISH_PROMPT_PREFIXES_SHOW false
-set SPACEFISH_PROMPT_DEFAULT_PREFIX " "
-set SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
-set SPACEFISH_USER_SHOW always
-set SPACEFISH_USER_COLOR green
-set SPACEFISH_HOST_SHOW always
-set SPACEFISH_HOST_COLOR cyan
-set SPACEFISH_DIR_COLOR blue
-set SPACEFISH_PROMPT_ORDER time user host dir git package node ruby golang php rust haskell julia elixir docker aws venv conda pyenv dotnet kubecontext exec_time line_sep battery vi_mode jobs exit_code char  
+starship init fish | source
